699dc25a97ee0ebb9ef1f520e2143e7597b85cf4
micke <micke@DESKTOP-M7TCG5B>
2023-09-12 13:24:02 +0200

    Add project files.

---
+++ BethanysPieShopHRM/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace BethanysPieShopHRM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int amount = 1234;
+            int months = 12;
+
+            int yearlyWage = CalculateYearlyWage(amount, months);
+            Console.WriteLine($"Yearly wage: {yearlyWage}");
+            Console.ReadLine();
+
+            static int CalculateYearlyWage(int monthlyWage, int numberOfMonthsWorked)
+            {
+                return monthlyWage * numberOfMonthsWorked;
+            }
+
+        }
+    }
+
+}
